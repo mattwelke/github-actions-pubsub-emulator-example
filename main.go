e65269cc4ff196277910c4df5367c897766e6dea
Matt Welke <mattwelke@gmail.com>
2022-02-07 18:31:39 -0500

    sijknfguisbnfui (#2)
    * sijknfguisbnfui
    
    * change 1644271112
    
    * change 1644271175
    
    * change 1644271439
    
    * change 1644271600
    
    * change 1644271745
    
    * change 1644271968
    
    * change 1644272098
    
    * change 1644272440
    
    * change 1644273405
    
    * change 1644273855
    
    * change 1644274439
    
    * change 1644274589
    
    * change 1644275249
    
    * change 1644275296
    
    * change 1644275420
    
    * change 1644275747
    
    * change 1644275957
    
    * change 1644276287
    
    * change 1644276501
    
    * change 1644276552
    
    * change 1644276565
    
    * change 1644276581
    
    * change 1644276686

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+)
 
 func main() {
-	fmt.Printf("Hello, World!\n")
+	log.Printf("Starting.")
+
+	client, err := pubsub.NewClient(context.Background(), "testproject")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create topic and subscription to it
+	topic, err := client.CreateTopic(context.Background(), "topic1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sub, err := client.CreateSubscription(context.Background(), "subscription1", pubsub.SubscriptionConfig{
+		Topic: topic,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Publish
+	res := topic.Publish(context.Background(), &pubsub.Message{
+		Data: []byte("hello world"),
+	})
+
+	// Get publish results
+	msgID, err := res.Get(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Published message with ID: %s", msgID)
+
+	for3Seconds, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	numAcked := 0
+
+	// Use a callback to receive messages via subscription1.
+	err = sub.Receive(for3Seconds, func(ctx context.Context, m *pubsub.Message) {
+		log.Printf("Received message: %v", string(m.Data))
+		m.Ack() // Acknowledge that we've consumed the message.
+		log.Printf("Acked message.")
+		numAcked++
+		cancel()
+	})
+	if err != nil {
+		log.Println(err)
+	}
+
+	if numAcked == 1 {
+		log.Printf("Program completed.")
+	} else {
+		panic(fmt.Errorf("expected 1 message acked but none were acked"))
+	}
 }
