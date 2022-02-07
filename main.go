1ddfd2a0c7acf029749a016d1793e6ba9478d56a
Matt Welke <matt.welke@groupbyinc.com>
2022-02-07 18:19:17 -0500

    change 1644275957

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"log"
-	"time"
 
 	"cloud.google.com/go/pubsub"
 )
@@ -11,20 +10,17 @@ import (
 func main() {
 	log.Printf("Starting.")
 
-	stepDuration := time.Duration(10 * time.Second)
-
-	createClientCtx, cancelCreateClient := context.WithTimeout(context.Background(), stepDuration)
-	defer cancelCreateClient()
-	client, err := pubsub.NewClient(createClientCtx, "testproject")
+	client, err := pubsub.NewClient(context.Background(), "testproject")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Create topic and subscription to it
-	topic := client.Topic("topic1")
-	createSubCtx, cancelCreateSub := context.WithTimeout(context.Background(), stepDuration)
-	defer cancelCreateSub()
-	sub, err := client.CreateSubscription(createSubCtx, "subscription1", pubsub.SubscriptionConfig{
+	topic, err := client.CreateTopic(context.Background(), "topic1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sub, err := client.CreateSubscription(context.Background(), "subscription1", pubsub.SubscriptionConfig{
 		Topic: topic,
 	})
 	if err != nil {
@@ -32,25 +28,19 @@ func main() {
 	}
 
 	// Publish
-	publishCtx, cancelPublish := context.WithTimeout(context.Background(), stepDuration)
-	defer cancelPublish()
-	res := topic.Publish(publishCtx, &pubsub.Message{
+	res := topic.Publish(context.Background(), &pubsub.Message{
 		Data: []byte("hello world"),
 	})
 
 	// Get publish results
-	getPublishResCtx, cancelGetPublishRes := context.WithTimeout(context.Background(), stepDuration)
-	defer cancelGetPublishRes()
-	msgID, err := res.Get(getPublishResCtx)
+	msgID, err := res.Get(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("Published message with ID: %s", msgID)
 
 	// Use a callback to receive messages via subscription1.
-	receiveCtx, cancelReceive := context.WithTimeout(context.Background(), stepDuration)
-	defer cancelReceive()
-	err = sub.Receive(receiveCtx, func(ctx context.Context, m *pubsub.Message) {
+	err = sub.Receive(context.Background(), func(ctx context.Context, m *pubsub.Message) {
 		log.Printf("Received message: %v", m)
 		m.Ack() // Acknowledge that we've consumed the message.
 		log.Printf("Acked message.")
